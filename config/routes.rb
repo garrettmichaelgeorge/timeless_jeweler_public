7f81b273b784c6dde4c52c3e3634ecade6d27e46
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2019-12-11 16:07:09 -0500

    Update layout with dashboard demo and static home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :customers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'customers#index'
+  resources :customers
+
+  root 'static_pages#home'
+  get '/dashboard', to: 'dashboard#index'
 end
