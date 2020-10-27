c97e2acd1bf10ca217bb13a38e14cc526cb7cd8c
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2020-10-27 11:07:31 -0400

    Add mounting table

--- app/models/mounting.rb
+++ app/models/mounting.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: mountings
+#
+#  gemstone_id      :bigint           not null
+#  jewelry_piece_id :bigint           not null
+#
+# Indexes
+#
+#  index_mountings_on_gemstone_id       (gemstone_id)
+#  index_mountings_on_jewelry_piece_id  (jewelry_piece_id)
+#
 class Mounting < ApplicationRecord
   belongs_to :gemstone
   belongs_to :jewelry_piece
--- test/models/mounting_test.rb
+++ test/models/mounting_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: mountings
+#
+#  gemstone_id      :bigint           not null
+#  jewelry_piece_id :bigint           not null
+#
+# Indexes
+#
+#  index_mountings_on_gemstone_id       (gemstone_id)
+#  index_mountings_on_jewelry_piece_id  (jewelry_piece_id)
+#
 require 'test_helper'
 
 class MountingTest < ActiveSupport::TestCase
