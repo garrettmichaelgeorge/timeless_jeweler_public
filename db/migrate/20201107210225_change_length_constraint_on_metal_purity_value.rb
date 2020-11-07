9f20bfdeb7c5010558d37df10594c41925911d8a
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2020-11-07 16:18:21 -0500

    Update metal purities to allow Unknown value

--- db/migrate/20201107210225_change_length_constraint_on_metal_purity_value.rb
+++ db/migrate/20201107210225_change_length_constraint_on_metal_purity_value.rb
@@ -1,5 +1,13 @@
 class ChangeLengthConstraintOnMetalPurityValue < ActiveRecord::Migration[6.0]
   def change
-    change_column :metal_purities, :value, :string, length: 5
+    reversible do |dir|
+      dir.up do
+        change_column :metal_purities, :value, :string, length: 10
+      end
+
+      dir.down do
+        change_column :metal_purities, :value, :string, length: 4
+      end
+    end
   end
 end
