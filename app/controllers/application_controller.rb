b1a21522f4a069659b450523b1e95dae6fa7a629
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-01 10:37:04 -0500

    Extract Pundit logic from ApplicationController => concerns/Punditable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,6 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  include Punditable
 
   before_action :authenticate_user!
-
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
-  private
-
-  def user_not_authorized(exception)
-    flash[:alert] = pundit_error_message_for(exception)
-    redirect_to(request.referrer || root_path)
-  end
-
-  def pundit_error_message_for(exception)
-    t error_message_path(exception), scope: 'pundit', default: :default
-  end
-
-  def error_message_path(exception)
-    "#{policy_name_from(exception)}.#{query_from(exception)}"
-  end
-
-  def policy_name_from(exception)
-    exception.policy.class.to_s.underscore
-  end
-
-  def query_from(exception)
-    exception.query
-  end
 end
