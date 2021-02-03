c13a3fa93fbc5e53fd243ebb8c9c7dd90561114c
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-03 10:53:51 -0500

    Add tests for Buttons::RemoveNestedFieldsComponent

---
+++ test/components/buttons/remove_nested_field_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Buttons::RemoveNestedFieldComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Buttons::RemoveNestedFieldComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
