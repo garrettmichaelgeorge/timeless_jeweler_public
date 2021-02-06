da1ec6cc71f6f0898c985070f630fe047891175b
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-06 11:43:55 -0500

    Add models and views subdirectories to config/locale

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 require 'view_component/engine'
 
 ActiveSupport::Deprecation.silenced = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -19,6 +20,15 @@ module TimelessJeweler
     # the framework and any gems in your application.
 
     config.active_record.schema_format = :sql
+
+    # Handle server error responses with the Rails router instead of serving
+    # static pages like public/422.html, public/500.html, etc.
+    # Errors are handled by ErrorsController
     config.exceptions_app = routes
+
+    # Allow for subdirectories within config/locales
+    # This is for e.g. config/locales/models and config/locales/views
+    # Read more: https://rails.rubystyle.guide/#organize-locale-files
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   end
 end
