e553793bd9a859cf0506e94b809ce87dd60f3150
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-08 16:34:39 -0500

    Turn off automatic placeholder generation in forms (for now)

--- config/initializers/simple_form_placeholder_monkey_patch.rb
+++ config/initializers/simple_form_placeholder_monkey_patch.rb
@@ -12,26 +12,26 @@ module SimpleForm
       # SimpleForm auto-generated labels implementation
       # https://github.com/heartcombo/simple_form/blob/cbcfe2e972fc25a53474d72b256a7f6c389de545/lib/simple_form/components/labels.rb#L73
 
-      def placeholder_text(_wrapper_options = nil)
-        placeholder = options[:placeholder]
+      # def placeholder_text(_wrapper_options = nil)
+      #   placeholder = options[:placeholder]
 
-        # Placeholder generation algorithm
-        # 1. Use provided placeholder provided it is a string
-        #    => OPTIMIZE: is this type check really necessary?
-        # 2. Look up translation in I18n
-        # 3. Generate placeholder by humanizing the attribute name specific to the form object
-        # 4. Generate placeholder by humanizing the attribute name generically
+      #   # Placeholder generation algorithm
+      #   # 1. Use provided placeholder provided it is a string
+      #   #    => OPTIMIZE: is this type check really necessary?
+      #   # 2. Look up translation in I18n
+      #   # 3. Generate placeholder by humanizing the attribute name specific to the form object
+      #   # 4. Generate placeholder by humanizing the attribute name generically
 
-        if placeholder.is_a?(String)
-          placeholder
-        elsif translate_from_namespace(:placeholders)
-          translate_from_namespace(:placeholders)
-        elsif object.class.respond_to?(:human_attribute_name)
-          object.class.human_attribute_name(reflection_or_attribute_name.to_s)
-        else
-          attribute_name.to_s.humanize
-        end
-      end
+      #   if placeholder.is_a?(String)
+      #     placeholder
+      #   elsif translate_from_namespace(:placeholders)
+      #     translate_from_namespace(:placeholders)
+      #   elsif object.class.respond_to?(:human_attribute_name)
+      #     object.class.human_attribute_name(reflection_or_attribute_name.to_s)
+      #   else
+      #     attribute_name.to_s.humanize
+      #   end
+      # end
     end
   end
 end
