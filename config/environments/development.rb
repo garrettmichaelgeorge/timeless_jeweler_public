270b5a826559ff265a50dc0c2da80f437978705e
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-09 16:49:05 -0500

    Use separate Redis instances for sessions and caches in development to mirror production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.session_store :cache_store
-
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -16,11 +14,14 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
+
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :redis_cache_store, { driver: :hiredis, url: ENV.fetch('REDIS_URL') do
+                                                                        'redis://localhost:6379/1'
+                                                                      end }
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
@@ -29,6 +30,10 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+
+  # Store sessions in Redis for compatibility with StimulusReflex
+  # https://docs.stimulusreflex.com/appendices/deployment#session-storage
+  config.session_store :cache_store, key: '_session_development', compress: true, pool_size: 5, expire_after: 1.year
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
