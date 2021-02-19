acdd695bc208588a03da8726a51f5e6c9e9ba922
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-19 17:39:34 -0500

    Add tests for Lookupable

--- app/models/concerns/lookupable.rb
+++ app/models/concerns/lookupable.rb
@@ -1,6 +1,10 @@
 module Lookupable
   extend ActiveSupport::Concern
 
+  included do
+    delegate :lookup_table?, :lookupable?, to: :class
+  end
+
   class_methods do
     def lookup_attribute(attr_name, maximum_length: 40)
       # NOTE: currently works only with arrays defined in lookup.en.yml
@@ -14,5 +18,10 @@ module Lookupable
       translator.t(to_s.pluralize.underscore.sub('/', '_'),
                    scope: :lookup).freeze
     end
+
+    def lookup_table?
+      true
+    end
+    alias_method :lookupable?, :lookup_table?
   end
 end
