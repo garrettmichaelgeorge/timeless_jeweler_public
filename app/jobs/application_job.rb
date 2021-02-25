b9a13a6c041d535a61d857b917a6132c7c3b55c3
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-25 16:50:00 -0500

    Set up Application Job to use Cable Ready

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -2,6 +2,7 @@ class ApplicationJob < ActiveJob::Base
   # Use CableReady from ActiveJob
   # https://cableready.stimulusreflex.com/cableready-everywhere#jobs
   include CableReady::Broadcaster
+
   delegate :render, to: :ApplicationController
 
   # Automatically retry jobs that encountered a deadlock
