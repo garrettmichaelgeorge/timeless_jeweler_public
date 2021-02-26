daa46dabad5c6af46c1c81a19e50df5db00fb221
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-26 10:12:59 -0500

    Fix db/seeds.rb MetalCategory => Metal::Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# Setup
+
 require 'rails-i18n'
 require_relative 'wiper'
 # require_relative 'seeder'
@@ -22,8 +24,8 @@ def seed_table(lookup_key, **opts)
   model = opts.fetch(:model) { lookup_key.constantize }
 
   if lookup(lookup_key).is_a?(Hash)
-    lookup(lookup_key).each_pair do |code, name|
-      model.create_or_find_by(name: name, code: code)
+    lookup(lookup_key).each do |code, name|
+      model.create_or_find_by(code: code, name: name)
     end
   else
     lookup(lookup_key).each do |name|
@@ -33,6 +35,8 @@ def seed_table(lookup_key, **opts)
 
   print '.'
 end
+
+# Begin
 
 # Wiper.execute
 
@@ -45,9 +49,9 @@ ActiveRecord::Base.transaction do
   seed_table(:diamond_clarities, model: Diamond::Clarity)
   seed_table(:diamond_cuts, model: Diamond::Cut)
   seed_table(:diamond_colors, model: Diamond::Color)
-  seed_table(:metal_categories, model: MetalCategory)
-  seed_table(:metal_colors, model: MetalColor)
-  seed_table(:metal_purities, model: MetalPurity)
+  seed_table(:metal_categories, model: Metal::Category)
+  seed_table(:metal_colors, model: Metal::Color)
+  seed_table(:metal_purities, model: Metal::Purity)
   seed_table(:piece_subcategories, model: Piece::Subcategory)
   seed_table(:state_provinces, model: StateProvince)
 end
