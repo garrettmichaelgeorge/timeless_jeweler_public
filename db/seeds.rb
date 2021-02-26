ef7aab33ee9bc94e867d350d10aa1f99e6837355
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-02-26 12:38:56 -0500

    Remove obsolete code from db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
-# Setup
-
 require 'rails-i18n'
-require_relative 'wiper'
-# require_relative 'seeder'
+# require_relative 'wiper'
 
-# module Seeds
-#   def self.seed_db_for(env)
-#     puts '== Database: seeding'
-#     Wiper.execute unless env.production?
-#     Seeder.execute
-#   end
-# end
-
-# Seeds.seed_db_for(Rails.env)
+# Setup
 
 def lookup(value)
   I18n.t(value, scope: :lookup)
