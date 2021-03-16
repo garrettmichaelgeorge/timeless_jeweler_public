92ddf46436c11b150bd6e8d89906238bd10b5ffa
garrettmichaelgeorge <garrett.michael.george@gmail.com>
2021-03-16 16:00:03 -0400

    Configure SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,28 @@
 # https://github.com/simplecov-ruby/simplecov
 require 'simplecov'
-SimpleCov.start
+
+SimpleCov.start 'rails' do
+  enable_coverage :branch
+  add_filter "/vendor/"
+  add_filter "/node_modules/"
+  add_filter "/lib/simple_form/"
+  add_filter "/lib/sti_preload"
+
+  add_group "Components", "app/components"
+  add_group "Reflexes", "app/reflexes"
+  add_group "Validators", "app/validators"
+
+  # If running test in CI, generate just .json result, then we can join them later
+  # else, generate the full HTML report
+  formatter SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::SimpleFormatter,
+      SimpleCov::Formatter::HTMLFormatter
+    ]
+  )
+
+  track_files "**/*.rb"
+end
 
 require_relative 'test_helper_lite'
 require_relative '../config/environment'
